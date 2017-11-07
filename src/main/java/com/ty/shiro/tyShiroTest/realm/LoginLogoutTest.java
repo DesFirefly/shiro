b5db65a2f1cab3da086f8d4adc96eaf0c5160e19
DesFirefly <desfirefly@163.com>
2017-11-07 22:25:32 +0800

    更新了MD5验证登录的方法

--- src/main/java/com/ty/shiro/tyShiroTest/realm/LoginLogoutTest.java
+++ src/main/java/com/ty/shiro/tyShiroTest/realm/LoginLogoutTest.java
@@ -30,13 +30,74 @@ public class LoginLogoutTest {
 		 
 		 try{
 			 subject.login(token);
-			 System.out.println("yanzhengchenggong");
+			 System.out.println("验证成功");
 		 } catch (AuthenticationException e){
-			 System.out.println("yanzhengshibai");
+			 System.out.println("验证失败");
 		 }
 		 
 		 Assert.assertEquals(true, subject.isAuthenticated());
 		 
 		 subject.logout();
 	}
+	
+	@Test
+    public void testCustomRealm() {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<org.apache.shiro.mgt.SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        try {
+            //4、登录，即身份验证
+            subject.login(token);
+            System.out.println("验证ok");
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+            e.printStackTrace();
+            System.out.println("验证falure");
+        }
+
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+
+        //6、退出
+        subject.logout();
+        System.out.println("logout");
+    }
+	
+	@Test
+    public void testCustomRealmMd5() {
+        //1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory =
+                new IniSecurityManagerFactory("classpath:shiro-realm-md5.ini");
+
+        //2、得到SecurityManager实例 并绑定给SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+
+        //3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
+
+        try {
+            //4、登录，即身份验证
+            subject.login(token);    
+            System.out.println("yes");
+        } catch (AuthenticationException e) {
+            //5、身份验证失败
+            e.printStackTrace();
+            System.out.println("no");
+        }
+
+        Assert.assertEquals(true, subject.isAuthenticated()); //断言用户已经登录
+
+        //6、退出
+        subject.logout();
+    }
 }
